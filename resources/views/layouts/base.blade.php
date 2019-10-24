f7f0c21e34ceb2f2dead2f738b7d076c66377563
iWeya <lucasvinicius211@yahoo.com>
2019-10-24 13:15:21 -0300

    Criar layout básico para a aplicação

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html class=" @yield('html-element-classes') " lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>
+        @hasSection('title')
+            @yield('title') | {{ config('app.name') }}
+        @else
+            {{ config('app.name') }}
+        @endif
+    </title>
+
+    @stack('css')
+</head>
+
+<body class=" @yield('body-element-classes') ">
+    @yield('content')
+
+    @stack('scripts')
+</body>
+
+</html>
