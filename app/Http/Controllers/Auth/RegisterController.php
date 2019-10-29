426ad81aa6e61cdac2cfe6e2a32bccce8cf9be7b
iWeya <lucasvinicius211@yahoo.com>
2019-10-29 13:40:46 -0300

    Criar métodos para requisição dos dados à api do IFRN

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 class RegisterController extends Controller
 {
@@ -41,6 +42,55 @@ class RegisterController extends Controller
     }
 
     /**
+     * Make a request to IFRN Open API to get user data
+     *
+     * @param string $username
+     * @return array
+     * @throws \Iluminate\Validation\ValidationException
+     **/
+    protected function requestDataFromApi(string $username)
+    {
+        $studentResourceId = config('constants.student_resource_id');
+
+        $requestHttpParameters = http_build_query([
+            'resource_id' => $studentResourceId,
+            'q' => $username,
+            'limit' => 1
+        ]);
+
+        $requestUrl = config('constants.ifrn_open_data_api_url');
+        $requestUrl .= '?'.$requestHttpParameters;
+
+        $apiData = file_get_contents($requestUrl);
+        $apiData = json_decode($apiData, true);
+
+        return $apiData;
+    }
+
+    /**
+     * Get user data from IFRN Open API
+     *
+     * @param string $username
+     * @return array
+     * @throws \Illuminate\Validation\ValidationException
+     **/
+    protected function getUserData(string $username)
+    {
+        $apiData = $this->requestDataFromApi($username);
+        $resultData = $apiData['result'];
+
+        if (empty($resultData['records'])) {
+            ValidationException::withMessages([
+                'username' => 'Not recognized'
+            ]);
+        }
+
+        $userData = $resultData['records'][0];
+
+        return $userData;
+    }
+
+    /**
      * Get a validator for an incoming registration request.
      *
      * @param  array  $data
@@ -64,6 +114,8 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        $personalUserData = $this->getUserData($data['username']);
+
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
