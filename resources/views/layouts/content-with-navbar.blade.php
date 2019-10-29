83c9797cac21da79ed25a95d6a0f9c7b125356a3
iWeya <lucasvinicius211@yahoo.com>
2019-10-29 13:43:10 -0300

    Criar layout para views com barra de navegação

---
+++ resources/views/layouts/content-with-navbar.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.base')
+
+@push('css')
+    <link rel="stylesheet" href="{{ asset('assets/css/e-ponto-bulma-theme/css/e-ponto-bulma-theme.css') }}">
+@endpush
+
+@section('content')
+    @component('components.navbar')
+        @slot('color', 'primary')
+        @slot('fixed', 'top')
+    @endcomponent
+
+    <div class="container">
+        @yield('main-content')
+        a
+    </div>
+@endsection
