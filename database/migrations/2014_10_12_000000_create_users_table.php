65a1030776e03e10baa27c755cad616c4d2a9939
iWeya <lucasvinicius211@yahoo.com>
2019-10-30 17:42:13 -0300

    Adicionar coluna "role_id" à tabela de usuários

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,10 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles');
+
             $table->timestamps();
         });
     }
