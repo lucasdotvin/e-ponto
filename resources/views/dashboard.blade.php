aefc421dd6a6c3a76366fa18403c84323138b7a4
iWeya <lucasvinicius211@yahoo.com>
2019-10-30 14:10:05 -0300

    Criar display do cumprimento de carga horária

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,49 @@
 @extends('layouts.content-with-navbar')
 
 @section('title', 'Dashboard')
+
+@section('main-content')
+    <div class="box">
+        <h2 class="title is-5">
+            Cumprimento de Carga Horária
+        </h2>
+
+        <main class="content">
+            <div class="field is-grouped ep-is-vcentered">
+                <div class="control is-expanded">
+                    <progress class="progress is-medium is-primary" max="100" value="75">
+                        75
+                    </progress>
+                </div>
+
+                <div class="control">
+                    <span class="tag">
+                        75%
+                    </span>
+                </div>
+            </div>
+
+            <div class="field">
+                <div class="control">
+                    <details>
+                        <summary class="ep-is-pointer">
+                            Mais detalhes
+                        </summary>
+
+                        <div class="content">
+                            <hr>
+
+                            <p>
+                                Carga Horária Total: <strong>100h</strong>.
+                            </p>
+
+                            <p>
+                                Carga Horária Cumprida: <strong>75h</strong>.
+                            </p>
+                        </div>
+                    </details>
+                </div>
+            </div>
+        </main>
+    </div>
+@endsection
