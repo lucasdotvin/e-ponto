899ea6f93f114f8333c361b932f050197e66cb97
iWeya <lucasvinicius211@yahoo.com>
2019-11-19 14:24:40 -0300

    Registrar rota para página de autorização do E-Ponto

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,7 @@ Route::group(['middleware' => ['auth']], function () {
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/authorize', function () {
+    return view('auth.authorize');
+});
