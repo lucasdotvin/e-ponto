e9de77af2a27a83e9ff2ab023226d00ef2fdae3e
iWeya <lucasvinicius211@yahoo.com>
2019-11-20 16:49:31 -0300

    Criar controlador de logout

---
+++ app/Http/Controllers/Auth/ClientLogoutController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
+
+class ClientLogoutController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        Auth::logout();
+        Cookie::queue(
+            Cookie::forget('suapToken')
+        );
+
+        return redirect('authorize');
+    }
+}
