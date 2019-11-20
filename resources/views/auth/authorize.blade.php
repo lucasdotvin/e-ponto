27df317f645a6aa555ff1d042c1ef5950f7d7464
iWeya <lucasvinicius211@yahoo.com>
2019-11-20 13:02:02 -0300

    Inserir jQuery na página

--- resources/views/auth/authorize.blade.php
+++ resources/views/auth/authorize.blade.php
@@ -31,6 +31,10 @@
         var SUAP_URL = '{!! env('SUAP_URL') !!}';
     </script>
 
+    <!-- <jQuery> -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <!-- </jQuery> -->
+
     <script src="{{ asset('assets/js/suap-api/js.cookie.js') }}"></script>
 
     <!-- <SUAP API Client> -->
