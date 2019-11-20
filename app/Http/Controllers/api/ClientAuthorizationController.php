3a55f825107908fab1f42ebbc129853ef258a293
iWeya <lucasvinicius211@yahoo.com>
2019-11-20 13:03:56 -0300

    Criar controlador para a autorização via API

---
+++ app/Http/Controllers/api/ClientAuthorizationController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ClientAuthorizationController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+    }
+}
