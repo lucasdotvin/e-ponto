35d1eb70356ced89f0af297dd090f02f2113fad4
iWeya <lucasvinicius211@yahoo.com>
2019-11-20 16:51:35 -0300

    Adicionar constante da rota de autorização do E-Ponto

--- resources/views/auth/authorize.blade.php
+++ resources/views/auth/authorize.blade.php
@@ -31,6 +31,10 @@
         var SUAP_URL = '{!! config('constants.suap_url') !!}';
     </script>
 
+    <script>
+        var EPONTO_API_AUTHORIZATION_URL = '{!! route('authorization') !!}';
+    </script>
+
     <!-- <jQuery> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <!-- </jQuery> -->
