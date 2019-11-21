ed96fdd4f9a28cabce7852d0d9ec7b005a0b1dc3
iWeya <lucasvinicius211@yahoo.com>
2019-11-21 20:08:42 -0300

    Criar versão da dashboard para visitantes

---
+++ resources/views/roles/guest/dashboard.blade.php
@@ -0,0 +1,23 @@
+@section('main-content')
+    <div class="box">
+        <h2 class="title is-5">
+            Boas-Vindas!
+        </h2>
+
+        <main class="content">
+            <p>
+                Sua conta não foi associada a nenhuma das funções deste sistema.
+                Caso acredite que isso se trata de um equívoco, procure o serviço social.
+                Outras dúvidas podem ser encaminhadas ao seguinte e-mail:
+
+                <a href="mailto:suporte.eponto@gmail.com">
+                    suporte.eponto@gmail.com
+                </a>
+            </p>
+
+            <p class="has-text-right is-italic">
+                Equipe do E-Ponto.
+            </p>
+        </main>
+    </div>
+@endsection
