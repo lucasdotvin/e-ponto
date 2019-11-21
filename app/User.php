c1de14aeb0860fe5a7f93cbb2554f548297147ae
iWeya <lucasvinicius211@yahoo.com>
2019-11-21 20:06:00 -0300

    Registrar relacionamento ao model user

--- app/User.php
+++ app/User.php
@@ -36,4 +36,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get the role of the user.
+     */
+    public function role()
+    {
+        return $this->belongsTo('App\Role');
+    }
 }
