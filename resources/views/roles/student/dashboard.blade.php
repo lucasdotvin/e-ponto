304cd4bb3491b69faa609906b24e5b1026885658
iWeya <lucasvinicius211@yahoo.com>
2019-11-21 20:08:48 -0300

    Criar versão do dashboard para estudantes

---
+++ resources/views/roles/student/dashboard.blade.php
@@ -0,0 +1,67 @@
+@section('navbar-start-items')
+    <a class="navbar-item">
+        <span class="icon">
+            <i class="fas fa-clock"></i>
+        </span>
+
+        <span>
+            Registrar Ponto
+        </span>
+    </a>
+
+    <a class="navbar-item">
+        <span class="icon">
+            <i class="fas fa-bell-slash"></i>
+        </span>
+
+        <span>
+            Notificações
+        </span>
+    </a>
+@endsection
+
+@section('main-content')
+    <div class="box">
+        <h2 class="title is-5">
+            Cumprimento de Carga Horária
+        </h2>
+
+        <main class="content">
+            <div class="field is-grouped ep-is-vcentered">
+                <div class="control is-expanded">
+                    <progress class="progress is-medium is-primary" max="100" value="75">
+                        75
+                    </progress>
+                </div>
+
+                <div class="control">
+                    <span class="tag">
+                        75%
+                    </span>
+                </div>
+            </div>
+
+            <div class="field">
+                <div class="control">
+                    <details>
+                        <summary class="ep-is-pointer">
+                            Mais detalhes
+                        </summary>
+
+                        <div class="content">
+                            <hr>
+
+                            <p>
+                                Carga Horária Total: <strong>100h</strong>.
+                            </p>
+
+                            <p>
+                                Carga Horária Cumprida: <strong>75h</strong>.
+                            </p>
+                        </div>
+                    </details>
+                </div>
+            </div>
+        </main>
+    </div>
+@endsection
