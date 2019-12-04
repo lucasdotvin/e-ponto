cd88780bc780e835aab7f98b372942f3b2330152
iWeya <lucasvinicius211@yahoo.com>
2019-12-04 18:49:23 -0300

    Criar rota para atualização dos dados de um registro de ponto

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::group(['middleware' => ['auth']], function () {
     Route::get('/punch-in-logs', 'PunchInLogController@index')->name('punch-in-log-index');
     Route::get('/punch-in-log/{punch_in_log}', 'PunchInLogController@show')->name('punch-in-log-show');
     Route::get('/punch-in-log/{punch_in_log}/edit', 'PunchInLogController@edit')->name('punch-in-log-edit');
+    Route::put('/punch-in-log/{punch_in_log}', 'PunchInLogController@update')->name('punch-in-log-update');
 });
 
 Route::get('/', function () {
