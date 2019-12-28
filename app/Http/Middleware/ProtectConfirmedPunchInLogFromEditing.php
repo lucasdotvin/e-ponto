ec77a77875253516ce204292c7a7a2191e5b092e
Lucas Vinicius <lucasvinicius211@yahoo.com>
2019-12-28 18:19:08 -0300

    Criar middleware para proteção de declarações de ponto confirmadas

---
+++ app/Http/Middleware/ProtectConfirmedPunchInLogFromEditing.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\PunchInLog;
+use Closure;
+
+class ProtectConfirmedPunchInLogFromEditing
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $punchInLogUuid = $request->punch_in_log;
+        $punchInLog = PunchInLog::findByUuid($punchInLogUuid);
+
+        if ($punchInLog->confirmed_by) {
+            return redirect(route('punch-in-log.show', $punchInLog));
+        }
+
+        return $next($request);
+    }
+}
