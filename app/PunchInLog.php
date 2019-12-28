d797290997f9ffc6c8f847e3d9e32bd421e5e169
Lucas Vinicius <lucasvinicius211@yahoo.com>
2019-12-28 18:20:00 -0300

    Registrar campos habilitados para preenchimento em massa

--- app/PunchInLog.php
+++ app/PunchInLog.php
@@ -8,4 +8,13 @@ use YourAppRocks\EloquentUuid\Traits\HasUuid;
 class PunchInLog extends Model
 {
     use HasUuid;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'work_day', 'work_start_time', 'work_end_time', 'work_total_time'
+    ];
 }
