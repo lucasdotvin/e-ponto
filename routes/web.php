ffc60c8a6a2b1ddc93302ddf505ba08a9ab7c9c9
Lucas Vinicius <lucasvinicius211@yahoo.com>
2019-12-28 18:30:18 -0300

    Registrar novas rotas e corrigir os nomes de todas

--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,16 @@ Route::group(['middleware' => ['auth']], function () {
 
     Route::get('/dashboard', 'DashboardController')->name('dashboard');
 
-    Route::get('/punch-in-logs', 'PunchInLogController@index')->name('punch-in-log-index');
-    Route::get('/punch-in-log/{punch_in_log}', 'PunchInLogController@show')->name('punch-in-log-show');
-    Route::get('/punch-in-log/{punch_in_log}/edit', 'PunchInLogController@edit')->name('punch-in-log-edit');
-    Route::put('/punch-in-log/{punch_in_log}', 'PunchInLogController@update')->name('punch-in-log-update');
+    Route::get('/punch-in-logs', 'PunchInLogController@index')->name('punch-in-log.index');
+    Route::get('/punch-in-logs/create', 'PunchInLogController@create')->name('punch-in-log.create');
+    Route::post('/punch-in-logs', 'PunchInLogController@store')->name('punch-in-log.store');
+    Route::get('/punch-in-log/{punch_in_log}', 'PunchInLogController@show')->name('punch-in-log.show');
+
+    Route::group(['middleware' => 'ProtectConfirmedPunchInLogFromEditing'], function () {
+        Route::get('/punch-in-log/{punch_in_log}/edit', 'PunchInLogController@edit')->name('punch-in-log.edit');
+        Route::put('/punch-in-log/{punch_in_log}', 'PunchInLogController@update')->name('punch-in-log.update');
+        Route::delete('/punch-in-log/{punch_in_log}', 'PunchInLogController@destroy')->name('punch-in-log.destroy');
+    });
 });
 
 Route::get('/', function () {
