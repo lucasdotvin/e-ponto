b31b38de7c4252297095de0f8e9d54e1e6f870af
Lucas Vinicius <lucasvinicius211@yahoo.com>
2019-12-28 18:14:54 -0300

    Criar model para a tabela "days_off"

---
+++ app/DayOff.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DayOff extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'days_off';
+}
