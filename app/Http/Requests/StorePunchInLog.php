6b787b6bb06740e43be888574c5e28b322614fae
Lucas Vinicius <lucasvinicius211@yahoo.com>
2019-12-28 18:17:39 -0300

    Criar request para validação de declarações de ponto

--- app/Http/Requests/StorePunchInLog.php
+++ app/Http/Requests/StorePunchInLog.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Carbon\Carbon;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StorePunchInLog extends FormRequest
@@ -14,9 +15,49 @@ class StorePunchInLog extends FormRequest
     public function rules()
     {
         return [
-            'work-day' => 'required|before_or_equal:now|date',
-            'work-start-time' => 'required|date_format:H\:i\:s',
-            'work-end-time' => 'required|date_format:H\:i\:s',
+            'work-day' => [
+                'required',
+                'before_or_equal:now',
+                'date'
+            ],
+
+            'work-start-time' => [
+                'required',
+                'date',
+                'before:now'
+            ],
+
+            'work-end-time' => [
+                'required',
+                'date',
+                'after:work-start-time',
+                'before_or_equal:now'
+            ],
+
+            'work-total-time' => [
+                'required'
+            ]
         ];
+    }
+
+    protected function prepareForValidation()
+    {
+        $workDay = $this->input('work-day');
+        $workStartTime = $this->input('work-start-time');
+        $workEndTime = $this->input('work-end-time');
+
+        $workStartDateTime = $workDay . ' ' . $workStartTime;
+        $workEndDateTime = $workDay . ' ' . $workEndTime;
+
+        $workTotalTime = new Carbon($workStartDateTime);
+        $workTotalTime = $workTotalTime->diffInMinutes(
+            $workEndDateTime
+        );
+
+        $this->merge([
+            'work-start-time' => $workStartDateTime,
+            'work-end-time' => $workEndDateTime,
+            'work-total-time' => $workTotalTime
+        ]);
     }
 }
