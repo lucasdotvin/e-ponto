5f2e18e426d0538f0e8958ef7499d5edac294a87
Lucas Vinicius <lucasvinicius211@yahoo.com>
2019-12-28 18:24:49 -0300

    Criar view de dashboard para coordenadores

---
+++ resources/views/roles/coordinator/dashboard.blade.php
@@ -0,0 +1,45 @@
+@section('main-content')
+    <div class="box">
+        <h2 class="title is-5">
+            Bolsistas
+        </h2>
+
+        <main class="content">
+            @if($departmentStudents->isEmpty())
+                <div class="has-text-centered">
+                    <p class="is-italic">
+                        Ainda não há bolsistas cadastrados.
+                    </p>
+                </div>
+            @endif
+
+            @foreach($departmentStudents as $student)
+                <article class="media">
+                    <section class="media-left">
+                        <span class="icon has-background-grey-lighter is-large">
+                            <i class="fas fa-user has-text-grey-light"></i>
+                        </span>
+                    </section>
+
+                    <main class="media-content">
+                        <div class="content">
+                            <strong>
+                                {{ $student->name }}
+                            </strong>
+
+                            <small>
+                                {{ print('@' . $student->username) }}
+                            </small>
+
+                            <br>
+
+                            <a href="">
+                                Visualizar dados
+                            </a>
+                        </div>
+                    </main>
+                </article>
+            @endforeach
+        </main>
+    </div>
+@endsection
