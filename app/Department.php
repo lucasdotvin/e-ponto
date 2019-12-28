29dcc94cf116efd030f8553e66be0a4eed4b0a29
Lucas Vinicius <lucasvinicius211@yahoo.com>
2019-12-28 18:15:23 -0300

    Registrar relacionamento com a tabela users

--- app/Department.php
+++ app/Department.php
@@ -6,4 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Department extends Model
 {
+    /**
+     * Get the users for the blog post.
+     */
+    public function users()
+    {
+        return $this->hasMany('App\User');
+    }
 }
