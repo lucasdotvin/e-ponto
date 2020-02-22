5e9198e57db8d56a010169634ce4428871fb9b7d
Lucas Vinicius <lucasvinicius211@yahoo.com>
2020-02-22 12:30:02 -0300

    Criar service de integração ao SUAP

---
+++ app/Services/SUAPClientService.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Services;
+use App\Contracts\AcademicSystemClientInterface;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
+
+class SUAPClientService implements AcademicSystemClientInterface
+{
+    public function getUserData (array $authenticationData): array {
+        $authToken = $authenticationData['token'];
+        $client = new Client([
+            'headers' => [
+                'Accept' => 'application/json',
+                'Authorization' => 'Bearer ' . $authToken
+            ]
+        ]);
+
+        $suapApiResourceUrl = config('suap.url');
+        $suapApiResourceUrl .= config('suap.api_resource_url');
+        $scope = $authenticationData['scope'];
+
+        $userData = $client->get($suapApiResourceUrl, [
+            'form_params' => [
+                'scope' => $scope
+            ]
+        ]);
+
+        $userData = $userData->getBody();
+        $userData = json_decode($userData, true);
+
+        return $userData;
+    }
+
+    public function logout (array $authenticationData): array {}
+}
