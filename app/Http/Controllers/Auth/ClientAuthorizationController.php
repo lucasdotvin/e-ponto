a8c6d173f9b6b3990b426a851d885b88cf9a62ec
Lucas Vinicius <lucasvinicius211@yahoo.com>
2020-02-22 12:30:18 -0300

    Aplicar service de integração ao SUAP no controller de autenticação

--- app/Http/Controllers/Auth/ClientAuthorizationController.php
+++ app/Http/Controllers/Auth/ClientAuthorizationController.php
@@ -4,43 +4,11 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\User;
-use GuzzleHttp\Exception\GuzzleException;
-use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class ClientAuthorizationController extends Controller
 {
-    /**
-     * Retrieve user data from SUAP API
-     *
-     * @param string $authToken
-     * @return array
-     **/
-    private function retrieveUserData(string $authToken, string $scope)
-    {
-        $client = new Client([
-            'headers' => [
-                'Accept' => 'application/json',
-                'Authorization' => 'Bearer ' . $authToken
-            ]
-        ]);
-
-        $suapApiResourceUrl = config('suap.url');
-        $suapApiResourceUrl .= config('suap.api_resource_url');
-
-        $userData = $client->get($suapApiResourceUrl, [
-            'form_params' => [
-                'scope' => $scope
-            ]
-        ]);
-
-        $userData = $userData->getBody();
-        $userData = json_decode($userData, true);
-
-        return $userData;
-    }
-
     /**
      * Handle the incoming request.
      *
@@ -49,15 +17,18 @@ class ClientAuthorizationController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $authToken = $request->input('token');
-        $scope = $request->input('scope');
+        $authenticationData = [
+            'token' => request()->input('token'),
+            'scope' => request()->input('scope')
+        ];
 
-        $apiData = $this->retrieveUserData($authToken, $scope);
+        $client = new \SUAPClient();
+        $userData = $client->getUserData($authenticationData);
 
         $user = User::firstOrCreate([
-            'email' => $apiData['email'],
-            'name' => $apiData['nome'],
-            'username' => $apiData['identificacao'],
+            'email' => $userData['email'],
+            'name' => $userData['nome'],
+            'username' => $userData['identificacao'],
         ]);
 
         return Auth::loginUsingId($user->id);
