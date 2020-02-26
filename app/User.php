82fca378048de8b72713a17b21b168fd7f4e4065
Lucas Vinicius <lucasvinicius211@yahoo.com>
2020-02-26 19:49:00 -0300

    Registrar uso de uuid no model User

--- app/User.php
+++ app/User.php
@@ -5,9 +5,11 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use YourAppRocks\EloquentUuid\Traits\HasUuid;
 
 class User extends Authenticatable
 {
+    use HasUuid;
     use Notifiable;
 
     /**
