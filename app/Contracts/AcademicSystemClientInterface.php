4498b48b348b29f740128162e776e0e3b7901e2e
Lucas Vinicius <lucasvinicius211@yahoo.com>
2020-02-26 19:38:32 -0300

    Remover parâmetros das definições de métodos do contrato

--- app/Contracts/AcademicSystemClientInterface.php
+++ app/Contracts/AcademicSystemClientInterface.php
@@ -7,16 +7,14 @@ interface AcademicSystemClientInterface
     /**
      * Coleta os dados do usuário no sistema.
      *
-     * @param array $authenticationData Dados de autenticação do usuário.
      * @return array|bool Dados do usuário ou false em caso de erro.
      **/
-    public function getUserData (array $authenticationData): array;
+    public function getUserData (): array;
 
     /**
      * Faz logout no sistema.
      *
-     * @param array $authenticationData Dados de autenticação do usuário.
      * @return bool Sucesso da operação.
      **/
-    public function logout (array $authenticationData): array;
+    public function logout ();
 }
