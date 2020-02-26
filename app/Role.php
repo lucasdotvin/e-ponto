56b2bd8340b01b7306b6fcaf2ab1721b224bcaf6
Lucas Vinicius <lucasvinicius211@yahoo.com>
2020-02-26 19:45:28 -0300

    Registrar relacionamento com o modelo User

--- app/Role.php
+++ app/Role.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    /**
+     * Get the users for the role.
+     */
+    public function users()
+    {
+        return $this->hasMany('App\User');
+    }
 }
