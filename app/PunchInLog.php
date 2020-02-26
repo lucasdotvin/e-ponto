934f5ebde3ba0af29b31dd299faa3d3b5c8df72c
Lucas Vinicius <lucasvinicius211@yahoo.com>
2020-02-26 19:45:10 -0300

    Registrar relacionamento com a tabela users

--- app/PunchInLog.php
+++ app/PunchInLog.php
@@ -17,4 +17,12 @@ class PunchInLog extends Model
     protected $fillable = [
         'work_day', 'work_start_time', 'work_end_time', 'work_total_time'
     ];
+
+    /**
+     * Get the user of the punch in log.
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'worker_id');
+    }
 }
