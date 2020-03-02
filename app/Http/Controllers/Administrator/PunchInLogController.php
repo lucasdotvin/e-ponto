fbac90a6cd024ca2e9eea6eceef67ac8bce7cca2
Lucas Vinicius <lucasvinicius211@yahoo.com>
2020-03-01 23:16:52 -0300

    Criar controller de registros de ponto de administrador

---
+++ app/Http/Controllers/Administrator/PunchInLogController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Administrator;
+
+use App\Http\Controllers\Controller;
+use App\PunchInLog;
+use App\User;
+
+class PunchInLogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param  string  $username
+     * @return \Illuminate\Http\Response
+     */
+    public function index($username)
+    {
+        $student = User::where('username', $username)->first();
+        return view('administrator.punch-in-log-index', [
+            'punchInLogs' => $student->punchInLogs,
+            'student' => $student
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  string  $uuid
+     * @return \Illuminate\Http\Response
+     */
+    public function show($uuid)
+    {
+        $punchInLog = PunchInLog::findByUuid($uuid);
+
+        return view('administrator.punch-in-log-show', [
+            'punchInLog' => $punchInLog
+        ]);
+    }
+}
