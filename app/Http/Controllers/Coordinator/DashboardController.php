b5d15503b17e9d739b92ae58f8536ca8b7317e01
Lucas Vinicius <lucasvinicius211@yahoo.com>
2020-03-01 23:17:15 -0300

    Limpar definição do controller

--- app/Http/Controllers/Coordinator/DashboardController.php
+++ app/Http/Controllers/Coordinator/DashboardController.php
@@ -3,11 +3,7 @@
 namespace App\Http\Controllers\Coordinator;
 
 use App\Department;
-use App\Helpers\BusinessDays;
 use App\Http\Controllers\Controller;
-use App\PunchInLog;
-use Carbon\Carbon;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
@@ -15,10 +11,9 @@ class DashboardController extends Controller
     /**
      * Handle the incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke()
     {
         $userDepartment = Auth::user()->department;
         $departmentStudents = Department::where('id', $userDepartment->id)
