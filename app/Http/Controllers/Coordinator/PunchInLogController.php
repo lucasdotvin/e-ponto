19973ccd6d4c57f643244c522742dc2c489e49e4
Lucas Vinicius <lucasvinicius211@yahoo.com>
2020-03-01 23:17:36 -0300

    Limpar definição do controller

--- app/Http/Controllers/Coordinator/PunchInLogController.php
+++ app/Http/Controllers/Coordinator/PunchInLogController.php
@@ -6,9 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StorePunchInLog;
 use App\PunchInLog;
 use App\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 
 class PunchInLogController extends Controller
 {
