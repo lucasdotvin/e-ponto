a7dcaaf8dc1f6d9152e3d50b67088987478c68fd
Lucas Vinicius <lucasvinicius211@yahoo.com>
2020-03-20 18:26:23 -0300

    Cria o controller de manipulação de relatórios

---
+++ app/Http/Controllers/Administrator/ReportController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Administrator;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreReport;
+use App\Report;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class ReportController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $reports = Report::all();
+        return view('administrator.report-index', [
+            'reports' => $reports
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('administrator.report-create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Requests\StoreReport $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreReport $request)
+    {
+        $input = $request->validated();
+        $month = new Carbon($input['month']);
+        $reportDataGenerator = new \ReportDataGenerator();
+
+        switch ($input['type']) {
+            case 'general':
+                $reportData = $reportDataGenerator->general(
+                    $month
+                );
+
+                break;
+        }
+
+        $reportTemplate = 'pdf.report.' . $input['type'];
+        $reportPDF = \PDF::loadView(
+            $reportTemplate,
+            compact('month', 'reportData')
+        );
+
+        return $reportPDF->stream();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
