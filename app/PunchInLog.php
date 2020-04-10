268b463696528d318adc35e60dd085c84fd7c0fe
Lucas Vinicius <lucasvinicius211@yahoo.com>
2020-04-10 19:01:41 -0300

    Cria manipuladores para os campos de hora de início de fim de trabalho
    Com esses manipuladores, os dados em formato datetime serão formatados para somente time, que é mais cabível dentro do projeto.

--- app/PunchInLog.php
+++ app/PunchInLog.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use YourAppRocks\EloquentUuid\Traits\HasUuid;
 
@@ -24,5 +25,33 @@ class PunchInLog extends Model
     public function user()
     {
         return $this->belongsTo('App\User', 'worker_id');
+    }
+
+    /**
+     * Get the punch in log's work start time.
+     *
+     * @param  string  $value
+     * @return Carbon
+     */
+    public function getWorkStartTimeAttribute($value)
+    {
+        $workStartTime = new Carbon($value);
+        $workStartTime = $workStartTime->toTimeString();
+
+        return $workStartTime;
+    }
+
+    /**
+     * Get the punch in log's work end time.
+     *
+     * @param  string  $value
+     * @return Carbon
+     */
+    public function getWorkEndTimeAttribute($value)
+    {
+        $workEndTime = new Carbon($value);
+        $workEndTime = $workEndTime->toTimeString();
+
+        return $workEndTime;
     }
 }
